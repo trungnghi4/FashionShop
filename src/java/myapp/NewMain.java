603a4cd6b9b72979ccb10dbe94fdf842d6df8788
QuanNguyen <abbadon.dl@gmail.com>
2019-05-16 00:22:38 +0700

    ss
    sss

---
+++ src/java/myapp/NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myapp;
+
+/**
+ *
+ * @author QuanNguyen
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a1 =2;
+        double a2 = 2.32;
+        int a3 = a1 + a2;
+        System.out.println(a3);
+    }
+    
+}
